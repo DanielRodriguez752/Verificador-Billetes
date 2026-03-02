a0ca8fc4dc2df221b42f4d91775af43839ed572b
DanielRodriguez752 <danicreed6@gmail.com>
2026-03-02 18:17:09 -0400

    InstalableV9

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-
+const CACHE_NAME = "verificador-v2";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
@@ -13,6 +14,18 @@ self.addEventListener("install", event => {
       ]);
     })
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
